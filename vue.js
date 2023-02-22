2a11a187cf073c72e972fed229a2913e8b69e86c
blueappleredappel <59490060+blueappleredappel@users.noreply.github.com>
2023-02-22 01:03:34 +0000

    tes

--- vue.js
+++ vue.js
@@ -13,17 +13,15 @@ class Vue {
         await this.c.loadMeteo();
         document.getElementById("tempNumber").innerHTML = Math.floor(this.c.temp);
         document.getElementById("weatherName").innerHTML = this.c.description;
-        document.getElementById("humidityNumber").innerHTML = this.c.humidity;
+        document.getElementById("humidity").innerHTML = this.c.humidity;
         document.getElementById("wind-number").innerHTML = this.c.wind;
         switch(this.c.description) {
             case "soleil":
-                document.getElementById("weatherImg").src = "/img/soleil.png"
+                document.getElementById("weatherImg").src = "vecteezy_weather-3d-icon_9386351_685-2 (2).png"
                 break;
             case "overcast clouds":
-                document.getElementById("weatherImg").src = "/img/cloudyForecast.png"
+                document.getElementById("weatherImg").src = "vecteezy_weather-3d-icon_9386351_685-4-2.png"
                 break;
-            default:
-                // code block
         }
         console.log(this.c.temp);
         console.log("hello");
