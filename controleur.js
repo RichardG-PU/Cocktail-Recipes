be0572a70ddd43d8475f42311d8f99a219cefbe3
javpot <105211675+javpot@users.noreply.github.com>
2023-03-01 10:43:51 +0000

    changeee

--- controleur.js
+++ controleur.js
@@ -2,10 +2,13 @@ class Controleur {
     api_key = '1ac298995ec2f7c641fb3806216c0c04';
     ville = document.getElementById("ville").value;
     url = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.ville + '&mode=xml&units=metric&appid=' + this.api_key;
+    urlDrinkAlea = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
     temp;
     description;
     humidity;
     wind;
+    drinkName;
+    drinkImage;
 
     loadMeteo() {
         return fetch(this.url)
@@ -22,9 +25,24 @@ class Controleur {
             }
             );
     }
+
+    obtenirRecetteAleatoire() {
+        return fetch(this.urlDrinkAlea).then((response) => response.json())
+            .then((data) => {
+                this.drinkName = data.drinks[0].strDrink;
+                this.drinkImage = data.drinks[0].strDrinkThumb;
+                console.log(this.drinkName);
+                return this;
+            });
+    }
 };
+
+
 window.onload = async function () {
     const c = await new Controleur().loadMeteo();
+    await c.obtenirRecetteAleatoire();
     const vue = new Vue(c);
     await vue.afficheMeteo();
+    await vue.afficheListeRecettes();
+
 };
--- vue.js
+++ vue.js
@@ -1,9 +1,13 @@
 class Vue {
     c;
-    temp = "yo";
+    temp;
     description;
     humidity;
     wind;
+    drinkName;
+    drinkImage;
+    compteur = 0;
+
 
     constructor(c) { // c is a controler
         this.c = c;
@@ -15,16 +19,24 @@ class Vue {
         document.getElementById("weatherName").innerHTML = this.c.description;
         document.getElementById("humidity").innerHTML = this.c.humidity;
         document.getElementById("wind-number").innerHTML = this.c.wind;
-        if(this.c.description.includes("sun") || this.c.description.includes("clear sky"))
+        if (this.c.description.includes("sun") || this.c.description.includes("clear sky"))
             document.getElementById("weatherImg").src = "vecteezy_weather-3d-icon_9386351_685-2-2.png";
-        else if(this.c.description.includes("cloud"))
+        else if (this.c.description.includes("cloud"))
             document.getElementById("weatherImg").src = "vecteezy_weather-3d-icon_9386351_685-5-2.png";
-        else if(this.c.description.includes("snow"))
+        else if (this.c.description.includes("snow"))
             document.getElementById("weatherImg").src = "vecteezy_weather-3d-icon_9386351_685_3.png";
 
     }
 
-    afficheListeRecettes() {
+    async afficheListeRecettes() {
+
+        while (this.compteur < 6) {
+            await this.c.obtenirRecetteAleatoire();
+            document.getElementById("roundText" + this.compteur).innerHTML = this.c.drinkName;
+            document.getElementById("r" + this.compteur).src = this.c.drinkImage;
+            console.log(this.c.drinkImage);
+            this.compteur++
+        }
 
     }
     afficheUneRecette() {
